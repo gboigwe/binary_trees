9f1c70a20d0f79e6223c3babdb7871c8056e9279
gboigwe <geakande@gmail.com>
2024-03-29 12:37:25 +0100

    Completed tasks on tree(balande, perfect, uncle)

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -7,13 +7,11 @@
  * Return: 0 if tree is NULL
  */
 
-int binary_tree_height(const binary_tree_t *tree)
+size_t binary_tree_height(const binary_tree_t *tree)
 {
-	int left_tree, right_tree;
+	size_t left_tree, right_tree;
 
 	if (tree == NULL)
-		return (0);
-	if (tree->left == NULL && tree->right == NULL)
 		return (0);
 
 	left_tree = binary_tree_height(tree->left);
